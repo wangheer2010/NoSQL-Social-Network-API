0923b3489c5ce80b78f328833eb6bf243e8150b8
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-18 21:14:11 -0400

    Thought Model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
