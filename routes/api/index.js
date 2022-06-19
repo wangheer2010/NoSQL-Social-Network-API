625964762fa74dae7d6b3c7f7891943ce02591c7
rabbid <50068816+wangheer2010@users.noreply.github.com>
2022-06-19 11:21:26 -0400

    added index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-//import routes from /api/index.js
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-//add api prefic to imported routes
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+// add users and thoughts to the beginning of their respective routes
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
